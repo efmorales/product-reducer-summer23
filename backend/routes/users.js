e98a1a7c88e99a82504c744a7b52add5f6d42549
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-06-01 09:03:19 -0700

    backend: added product route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/new', function(request, response, callback) {
+  response.send('New User Route')
+}) 
+
 module.exports = router;
