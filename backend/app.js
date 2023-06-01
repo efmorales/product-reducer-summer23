7a829c337431c401562123cdb7792680a2759610
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-06-01 10:33:16 -0700

    finished backend

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 // make a new route called store, it should contain a get for list-products thats sends an array []
-
+var storeRouter = require('./routes/store')
+ 
 
 var app = express();
 //
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/products', productsRouter);
+app.use('/api/store', storeRouter)
   
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
